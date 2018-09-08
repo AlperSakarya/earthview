d2d852d00d6081246c5a797315902457f6946576
Alper Sakarya <alper@mediaboil.com>
2018-09-07 21:26:27 -0400

    All the errors resolved, libraries updated for Ubuntu 18.04 & Python3

--- wallpaper-changer/indicator.py
+++ wallpaper-changer/indicator.py
@@ -1,6 +1,5 @@
 import signal
 import gi
-import time
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 gi.require_version('Notify', '0.7')
@@ -28,7 +27,7 @@ def main():
 def build_menu():
     menu = gtk.Menu()
     item_changewallpaper = gtk.MenuItem('Change Wallpaper')
-    item_changewallpaper.connect('activate', changewallpaper)
+    item_changewallpaper.connect('activate', change_wallpaper())
     menu.append(item_changewallpaper)
     item_quit = gtk.MenuItem('Quit')
     item_quit.connect('activate', code_quit)
@@ -41,7 +40,7 @@ def code_quit(source):
     gtk.main_quit()
 
 
-def changewallpaper(source):
+def change_wallpaper(source):
     urlFile = os.path.dirname(os.path.realpath(__file__)) + "/data.json"
     with open(urlFile) as data_file:
         data = json.load(data_file)
@@ -57,7 +56,6 @@ def changewallpaper(source):
     command = "gsettings set org.gnome.desktop.background picture-uri " + location
     os.system(command)
     notify.Notification.new("Earth View Wallpaper Changer", "Wallpaper changed successfully", None).show()
-    time.sleep(2)
 
 
 if __name__ == "__main__":
